039ad2da2ba0b15da7f1226ff1842d14bab42d80
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-03-15 11:13:53 +0530

    find-peak-element

---
+++ find-peak-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {    
+        if (nums.size() == 1) { // one index case
+            return 0;
+        }
+    
+   int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+};
