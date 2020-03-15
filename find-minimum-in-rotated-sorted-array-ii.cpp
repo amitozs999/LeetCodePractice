a03dae491507a24aa230cc68730a74d6e059f67a
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-03-15 11:12:05 +0530

    find-minimum-in-rotated-sorted-array-ii

---
+++ find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   int findMin(vector<int>& nums) {
+    int l = 0, r = nums.size()-1;
+    while (l < r) {
+        int mid = (r-l)/2 + l;
+        if (nums[mid] < nums[r])
+            r = mid;
+        else if(nums[mid]>nums[r])
+            l = mid + 1;
+        else
+            r--;
+    }
+    return nums[l];
+}
+};
