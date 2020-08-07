092ff48f8518aed203a251ddaab8396e0fa0c4a9
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2020-08-07 20:44:04 +0530

    k-diff-pairs-in-an-array

---
+++ k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        unordered_set<int> result, lookup;
+        for (const auto& num : nums) {
+            if (lookup.count(num - k)) {
+                result.emplace(num - k);
+            }
+            if (lookup.count(num + k)) {
+                result.emplace(num);
+            }
+            lookup.emplace(num);
+        }
+        return result.size();
+    }
+};
